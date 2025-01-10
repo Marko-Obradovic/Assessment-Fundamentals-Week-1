d28faf3f6e9b982b16273c898e67fa455cbe0593
marko-obradovic <marko@obradovic.co.uk>
2025-01-10 17:32:37 +0000

    for upload

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -24,13 +24,22 @@ def generate_receipt(basket: list) -> str:
 
     receipt = []
     item_appearance_count = {}
+    item_appearance_tracking = {}
     for item in basket:
         if item['price'] != "Free":
             if item['name'] not in item_appearance_count:
                 item_appearance_count[item["name"]] = 1
             else:
                 item_appearance_count[item["name"]] += 1
-            receipt.append(''.join(str(f"{item['name']} x{item_appearance_count[item["name"]]} - £{item["price"]:.2f}")))
+
+            formatted_item = ''.join(str(f"{item['name']} x {item_appearance_count[item["name"]]} - £{item["price"]:.2f}"))
+
+            if item['name'] in item_appearance_tracking:
+                receipt.remove(item_appearance_tracking[item["name"]])
+
+            receipt.append(formatted_item)
+
+            item_appearance_tracking[item["name"]] = formatted_item
         else:
             receipt.append(''.join(str(f"{item['name']} - Free")))
 
@@ -40,20 +49,11 @@ def generate_receipt(basket: list) -> str:
 
 
 if __name__ == "__main__":
-    add_to_basket({
-        "name": "Bread",
-        "price": 1.80
-    })
-    add_to_basket({
-        "name": "Bread",
-        "price": 1.80
-    })
-    add_to_basket({
-        "name": "Milk",
-        "price": 0.80
-    })
-    add_to_basket({
-        "name": "Butter",
-        "price": 1.20
-    })
+    item1 = {'name': 'Apple', 'price': 0.5}
+    item2 = {'name': 'Toffee', 'price': 0.3}
+    add_to_basket(item1)
+    add_to_basket(item1)
+    add_to_basket(item1)
+    add_to_basket(item2)
+    add_to_basket(item2)
     print(generate_receipt(basket))
