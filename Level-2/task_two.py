be7ef5212250fe6b21ab0f8804ccc882a3b4b74d
marko-obradovic <marko@obradovic.co.uk>
2025-01-10 16:56:47 +0000

    finished level-1

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -5,26 +5,38 @@ The only variable you are allowed to use in the global scope is the basket below
 
 basket = []
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 BELOW
-#
-#####
-
 
 def add_to_basket(item: dict) -> list:
-    pass
-
+    item["price"] = round(item["price"], 2)
+    basket.append(item)
+    return basket
 
 
 def generate_receipt(basket: list) -> str:
-    pass
+    if not basket:
+        return "Basket is empty"
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 ABOVE
-#
-#####
+    costs = [price["price"] for price in basket]
+    total = round(sum(costs), 2)
+    format_total = f"Total: £{total:.2f}"
+
+    print(basket)
+
+    receipt = []
+    item_appearance_count = {}
+    for item in basket:
+        if item['price'] != "Free":
+            if item['name'] not in item_appearance_count:
+                item_appearance_count[item["name"]] = 1
+            else:
+                item_appearance_count[item["name"]] += 1
+            receipt.append(''.join(str(f"{item['name']} x{item_appearance_count[item["name"]]} - £{item["price"]:.2f}")))
+        else:
+            receipt.append(''.join(str(f"{item['name']} - Free")))
+
+    receipt.append(format_total)
+    format_receipt = '\n'.join(receipt)
+    return format_receipt
 
 
 if __name__ == "__main__":
